cca9c457e859556faf18e88650ec0c7cd6801490
isaaccavallaro <isaaccavallaro@gmail.com>
2023-02-17 16:20:24 -0500

    User input

--- main.py
+++ main.py
@@ -4,4 +4,17 @@ suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 deck = [(suit, rank) for suit in suits for rank in ranks]
 
+while True:
+    hand_type = input('Enter the hand type (flush, full house, three of a kind, royal flush): ')
+    if hand_type not in ['flush', 'full house', 'three of a kind', 'royal flush']:
+        print('Invalid input. Please try again.')
+    else:
+        break
+
+while True:
+    try:
+        num_simulations = int(input('Enter the number of simulations: '))
+        break
+    except ValueError:
+        print('Invalid input. Please enter an integer.')
 
