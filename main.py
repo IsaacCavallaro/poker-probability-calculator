f6ab9b4f680a438614291cd399bf7d258bc91996
isaaccavallaro <isaaccavallaro@gmail.com>
2023-02-18 10:26:50 -0500

    is_flush function

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 deck = [(suit, rank) for suit in suits for rank in ranks]
 
+def is_flush(hand):
+    suits = [card[0] for card in hand]
+    if len(set(suits)) == 1:
+        return True
+    else:
+        return False
+
 while True:
     hand_type = input('Enter the hand type (flush, full house, three of a kind, royal flush): ')
     if hand_type not in ['flush', 'full house', 'three of a kind', 'royal flush']:
